628ecf45fbfd7a784b1103ce3585af7a6efa90f0
chomado <chomado@gmail.com>
2014-12-01 00:13:14 +0900

    同じような処理が何度も出てきていたので天気情報をclassにした. あと140字制限で一文短く.

--- bot.php
+++ bot.php
@@ -2,69 +2,94 @@
 require_once('twitteroauth/twitteroauth.php');
 require_once('botconfig.php');
 
+// 天気情報クラス
+class Weather
+{
+	private $city;
+	// {天気, {最高気温, 最低気温}}
+	private $info = [
+		'weather'=>'0', ['high'=>'1', 'low'=>'2']
+	];
+
+	public function __construct($city)
+	{
+		$this->city = $city;
+	}
+
+	// yahoo の天気予報 API から引っ張ってくる
+	private function GetWeather()
+	{
+		return json_decode(file_get_contents(
+			'https://query.yahooapis.com/v1/public/yql?q=select%09*%20from%20%09weather.forecast%20%20where%20%09woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22' 
+			. $this->city
+			. '%2C%20jp%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys'
+			))->query->results->channel->item;
+	}
+	// 華氏→摂氏変換関数
+	private function FtoC($f)
+	{
+		return round(($f - 32) * 0.555, 1);
+	}
+	// 現在の天気
+	public function GetCondition()
+	{
+		$now = $this->GetWeather($city)->condition;
+		$info['weather'] 	= $now->text;
+		$info[1]['high']	= $this->FtoC($now->temp);
+		return $info;
+	}
+	// 明日の天気情報
+	public function GetTomorrow()
+	{
+		$tomorrow = $this->GetWeather($city)->forecast[1];
+		$info['weather']	= $tomorrow->text;
+		$info[1]['high']	= $this->FtoC($tomorrow->high);
+		$info[1]['low'] 	= $this->FtoC($tomorrow->low);
+		return $info;
+	}
+}
+
 // 現在時刻. タイムゾーンはJST指定
 $time = new DateTime('now', new DateTimeZone('Asia/Tokyo'));
 
-// yahoo の天気予報 API から引っ張ってくる
-function GetWeather($city='tokyo')
-{
-    return json_decode(file_get_contents(
-        'https://query.yahooapis.com/v1/public/yql?q=select%09*%20from%20%09weather.forecast%20%20where%20%09woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22' 
-        . $city 
-        . '%2C%20jp%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys'
-        ));
-}
-// 現在の天気
-$weather = GetWeather()// いずれは都市をユーザが指定できるようにしたい
-            ->query->results->channel->item->condition->text; 
-// 現在の気温
-$temp = GetWeather()
-            ->query->results->channel->item->condition->temp;
-// 明日の天気
-$tomorrow_weather = GetWeather()
-            ->query->results->channel->item->forecast[1]->text;
-// 明日の最高気温
-$tomorrow_high_temp = GetWeather()
-            ->query->results->channel->item->forecast[1]->high;
-// 明日の最低気温
-$tomorrow_low_temp = GetWeather()
-            ->query->results->channel->item->forecast[1]->low;
-// 華氏→摂氏変換関数
-function FtoC($f)
-{
-    return round(($f - 32) * 0.555, 1);
-}
-
 // ファイルの行をランダムに抽出
-$filelist = file('list.txt');
-if ( shuffle($filelist) )
-{
-    $message = $filelist[0]  
-                . PHP_EOL 
-                . '東京の現在('
-                . $time->format('m/d H:i')
-                . ')の天気は'
-                . $weather
-                . 'です.'
-                . PHP_EOL
-                . '明日は'
-                . $tomorrow_weather
-                . 'で, '
-                . '最高気温は'
-                . FtoC($tomorrow_high_temp)
-                . '度, 最低気温は'
-                . FtoC($tomorrow_low_temp)
-                . '度です.';
-}
+$filelist 	= file('list.txt');
+shuffle($filelist);
+$message 	= $filelist[0] . PHP_EOL;
+
+// 現在の天気と明日の予報を入手
+$now_instance 		= new Weather('tokyo');
+$tomorrow_instance 	= new Weather('tokyo');
+
+$now 		= $now_instance->GetCondition();
+$tomorrow 	= $tomorrow_instance->GetTomorrow();
+
+// 呟く文に天気情報を加える
+$message	.= '東京の現在('
+			. $time->format('m/d H:i')
+			. ')の天気は'
+			. $now['weather']
+			. '('
+			. $now[1]['high']
+			. '℃)です.'
+			. PHP_EOL
+			. '明日は'
+			. $tomorrow["weather"]
+			. 'で, '
+			. '最高気温は'
+			. $tomorrow[1]['high']
+			. '℃, 最低気温は'
+			. $tomorrow[1]['low']
+			. '℃ です.';
 
 // Twitterに接続
 $connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET);
 
 $req = $connection -> OAuthRequest(
-    "https://api.twitter.com/1.1/statuses/update.json"
-    , "POST"
-    , array("status"=> $message )
-    );
+	"https://api.twitter.com/1.1/statuses/update.json"
+	, "POST"
+	, array("status"=> $message )
+	);
 
 var_dump($req);
 var_dump($message);
