bdc0f5e25699104d937975aed4d3ac403d86528f
chomado <chomado@gmail.com>
2015-01-19 21:04:28 +0900

    今年が始まってから何日経過したか呟く&140字制限引っ掛かるから少し削った

--- bot.php
+++ bot.php
@@ -11,6 +11,11 @@ $filelist 	= file('tweet_content_data_list/list.txt');
 shuffle($filelist);
 $message 	= $filelist[0] . PHP_EOL;
 
+// 経過日数%表示『今日で今年の約5.0%が過ぎました』
+$newYearsDay = new DateTime($time->format('Y') . '-01-01 00:00:00', new DateTimeZone('Asia/Tokyo'));
+$pastDays 	= $time->diff($newYearsDay)->format('%d');
+$message 	.= $pastDays . '日経過。';
+
 // 現在の天気と明日の予報を入手
 $weather	= new Weather('tokyo');
 
@@ -24,16 +29,16 @@ $message	.= '東京の現在('
 			. $now['weather']
 			. '('
 			. $now['temp']
-			. '℃)です.'
+			. '℃)です。'
 			. PHP_EOL
 			. '明日は'
 			. $tomorrow['weather']
-			. 'で, '
-			. '最高気温は'
+			. 'で，'
+			. '最高気温'
 			. $tomorrow['high']
-			. '℃, 最低気温は'
+			. '℃，最低気温'
 			. $tomorrow['low']
-			. '℃ です.';
+			. '℃ です。';
 
 // Twitterに接続
 $connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET);
