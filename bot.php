0c2d6dfafc71fbac1f5ec21761973d5e1e783813
chomado <chomado@gmail.com>
2015-01-20 00:33:06 +0900

    うるう年考慮

--- bot.php
+++ bot.php
@@ -3,52 +3,53 @@ require_once('twitteroauth/twitteroauth.php'); // OAuth
 require_once('static_data/botconfig.php'); // Twitterの各アクセスキー
 require_once('weather.php'); // Weatherクラスが入ってる
 
+// ファイルの行をランダムに抽出
+$filelist   = file('tweet_content_data_list/list.txt');
+shuffle($filelist);
+$message    = $filelist[0] . PHP_EOL;
+
 // 現在時刻. タイムゾーンはJST指定
 $time = new DateTime('now', new DateTimeZone('Asia/Tokyo'));
-
-// ファイルの行をランダムに抽出
-$filelist 	= file('tweet_content_data_list/list.txt');
-shuffle($filelist);
-$message 	= $filelist[0] . PHP_EOL;
-
-// 現在の天気と明日の予報を入手
-$weather	= new Weather('tokyo');
-
-$now 		= $weather->GetCondition();
-$tomorrow 	= $weather->GetTomorrow();
-
-// 呟く文に天気情報を加える
-$message	.= '東京の現在('
-            . $time->format('m/d H:i')
-            . ')の天気は'
-			. $now['weather']
-			. '('
-			. $now['temp']
-			. '℃)です。'
-			. PHP_EOL
-			. '明日は'
-			. $tomorrow['weather']
-			. 'で，'
-			. '最高気温'
-			. $tomorrow['high']
-			. '℃，最低気温'
-			. $tomorrow['low']
-			. '℃ です。'
-			. PHP_EOL;
-
 // 経過日数%表示『今日で今年の約5.0%が過ぎました』
 $newYearsDay = new DateTime($time->format('Y') . '-01-01 00:00:00', new DateTimeZone('Asia/Tokyo'));
-$pastDays 	= ($time->diff($newYearsDay)->d * 100) / 365;
-$message 	.= '今年' . round((string)$pastDays, 2) . '%経過';
+$lastDay    = new DateTime($time->format('Y') . '-12-31 23:59:00', new DateTimeZone('Asia/Tokyo'));
+$pastDays   = ($time->diff($newYearsDay)->days * 100) / ($time->diff($newYearsDay)->days + $lastDay->diff($time)->days);
+$message    .= '今日で今年の' . round($pastDays, 1) . '%が経過しました。'
+            . PHP_EOL;
+
+// 現在の天気と明日の予報を入手
+$weather    = new Weather('tokyo');
+
+$now        = $weather->GetCondition();
+$tomorrow   = $weather->GetTomorrow();
+
+// 呟く文に天気情報を加える
+$message    .= '東京の現在('
+            . $time->format('m/d H:i')
+            . ')の天気は'
+            . $now['weather']
+            . '('
+            . $now['temp']
+            . '℃)です。'
+            . PHP_EOL
+            . '明日は'
+            . $tomorrow['weather']
+            . 'で、'
+            . '最高'
+            . $tomorrow['high']
+            . '℃、最低'
+            . $tomorrow['low']
+            . '℃です'
+            . PHP_EOL;
 
 // Twitterに接続
 $connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET);
 
 $req = $connection -> OAuthRequest(
-	"https://api.twitter.com/1.1/statuses/update.json"
-	, "POST"
-	, array("status"=> $message )
-	);
+    "https://api.twitter.com/1.1/statuses/update.json"
+    , "POST"
+    , array("status"=> $message )
+    );
 
 //var_dump($req);
 var_dump($message);
