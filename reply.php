12a09c6c5a7d6b8e4d7cb2896445d14318d74dd8
chomado <chomado@gmail.com>
2015-01-22 19:43:13 +0900

    Revert "fix エンコード"
    This reverts commit 9b5220914c7d98abed322c6c2e3cafc4a3ac0063.

--- reply.php
+++ reply.php
@@ -33,7 +33,7 @@ if (!empty($res))
 			, $re->user->screen_name
 			, trim(preg_replace('!([@＠#＃.]|://)!u', " $1 ", $re->user->name))
 			, PHP_EOL
-			, mb_convert_encoding($message, "UTF-8", "auto");
+			, $message
 			);
 		$index = $index < count($reply_list) - 1 ? $index + 1 : 0;
 
