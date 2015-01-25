1c531bda16099207e8b61610f1a73132c77cb9e2
chomado <chomado@gmail.com>
2015-01-25 13:01:17 +0900

    docomoの対話API使って対話機能実装

--- reply.php
+++ reply.php
@@ -1,13 +1,18 @@
 <?php
 require_once('twitteroauth/twitteroauth.php'); // OAuth
 require_once('static_data/botconfig.php'); // Twitterの各アクセスキー
+require_once('class/chat.php'); // docomo対話APIのクラス
 
+var_dump('$param[since_id] : ' . PHP_EOL);
+var_dump($param['since_id']);
 // 最終投稿IDを取得
 $param['since_id'] = file_get_contents('tweet_content_data_list/last_id.txt');
 if (empty($param['since_id'])) {
 	$param = null;
 }
 
+var_dump('$param[since_id] : ' . PHP_EOL);
+var_dump($param['since_id']);
 // ファイルの行をランダムに抽出
 $reply_list = file('tweet_content_data_list/reply_list.txt');
 shuffle($reply_list);
@@ -26,8 +31,15 @@ if (!empty($res))
 		// もし自分自身宛てだったら無視する.(無限ループになっちゃうから)
 		if ($re->user->screen_name === 'chomado_bot')
 			continue;
+		
 		// ツイート本文
-		$message = $reply_list[$index];
+		$chat 		= new Chat($re->user->screen_name, $re->user->name, $re->text);
+		$message 	= sprintf('%s%s%s%s'
+			, $reply_list[$index]
+			, PHP_EOL
+			, $chat->ResText()
+			, PHP_EOL
+			);
 
 		$param['status'] = sprintf('@%s %sさん%s%s'
 			, $re->user->screen_name
@@ -42,5 +54,5 @@ if (!empty($res))
 		$connection->post('statuses/update', $param);
 	}
 	// 最終投稿IDを書き込む
-	file_put_contents('last_id.txt', $res[0]->id_str);
+	file_put_contents('tweet_content_data_list/last_id.txt', $res[0]->id_str);
 }
