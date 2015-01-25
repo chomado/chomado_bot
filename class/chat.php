cbcfb6391dc9b2b38c895e98e77b402b1de54f6c
chomado <chomado@gmail.com>
2015-01-25 13:11:31 +0900

    Convert Indentation to Spaeces

--- class/chat.php
+++ class/chat.php
@@ -4,48 +4,48 @@ require_once(dirname(__FILE__) . '/../static_data/botconfig.php'); // Twitterの
 
 // docomo の 雑談対話APIを使う
 // new Chat($context, $nickname, $text)
-	// 引数説明
-    // $context; 	// 会話のつながり保持のID. 
-    // $nickname;	// 名前
-    // $text;		// リプライで来た本文.
+    // 引数説明
+    // $context;    // 会話のつながり保持のID. 
+    // $nickname;   // 名前
+    // $text;       // リプライで来た本文.
 
 class Chat
 {
-	private $response;
+    private $response;
 
     public function __construct($context, $nickname, $text)
     {
-    	var_dump('送るテキスト:');
-    	var_dump($text);
+        var_dump('送るテキスト:');
+        var_dump($text);
         $this->response = $this->GetData($context, $nickname, $text);
     }
     // docomoの対話APIを叩いてレスポンスを貰ってくる (JSON形式)
     private function GetData($context, $nickname, $text)
     {
-    	$user_data = array(
-        	'utt' 		=> $text,
-        	'context' 	=> $context,
-        	'nickname'	=> $nickname,
+        $user_data = array(
+            'utt'       => $text,
+            'context'   => $context,
+            'nickname'  => $nickname,
         );
-		$url 		= sprintf(
-						'https://api.apigw.smt.docomo.ne.jp/dialogue/v1/dialogue?APIKEY=%s'
-						, DOCOMO_CHAT_API_KEY);
-    	$options 	= array('http' => array(
-		    'method' 	=> 'POST',
-		    'header' 	=> "Content-type: application/json",
-		    'content' 	=> json_encode($user_data),
-		));
-		$contents = json_decode(file_get_contents($url, false, stream_context_create($options)));
-		return $contents;
+        $url        = sprintf(
+                        'https://api.apigw.smt.docomo.ne.jp/dialogue/v1/dialogue?APIKEY=%s'
+                        , DOCOMO_CHAT_API_KEY);
+        $options    = array('http' => array(
+            'method'    => 'POST',
+            'header'    => "Content-type: application/json",
+            'content'   => json_encode($user_data),
+        ));
+        $contents = json_decode(file_get_contents($url, false, stream_context_create($options)));
+        return $contents;
     }
     public function ResText()
     {
-    	var_dump('返ってきたデータ:' . PHP_EOL);
-    	var_dump($this->response);
-    	$message = sprintf('%s%s'
-    		, $this->response->utt
-    		, PHP_EOL
-    		);
-    	return $message;
+        var_dump('返ってきたデータ:' . PHP_EOL);
+        var_dump($this->response);
+        $message = sprintf('%s%s'
+            , $this->response->utt
+            , PHP_EOL
+            );
+        return $message;
     }
 }
--- reply.php
+++ reply.php
@@ -8,7 +8,7 @@ var_dump($param['since_id']);
 // 最終投稿IDを取得
 $param['since_id'] = file_get_contents('tweet_content_data_list/last_id.txt');
 if (empty($param['since_id'])) {
-	$param = null;
+    $param = null;
 }
 
 var_dump('$param[since_id] : ' . PHP_EOL);
@@ -16,7 +16,7 @@ var_dump($param['since_id']);
 // ファイルの行をランダムに抽出
 $reply_list = file('tweet_content_data_list/reply_list.txt');
 shuffle($reply_list);
-$index 		= 0;
+$index      = 0;
 
 // Twitterに接続
 $connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET);
@@ -26,33 +26,33 @@ $res = $connection->get('statuses/mentions_timeline', $param);
 
 if (!empty($res))
 {
-	foreach ($res as $re) 
-	{
-		// もし自分自身宛てだったら無視する.(無限ループになっちゃうから)
-		if ($re->user->screen_name === 'chomado_bot')
-			continue;
-		
-		// ツイート本文
-		$chat 		= new Chat($re->user->screen_name, $re->user->name, $re->text);
-		$message 	= sprintf('%s%s%s%s'
-			, $reply_list[$index]
-			, PHP_EOL
-			, $chat->ResText()
-			, PHP_EOL
-			);
+    foreach ($res as $re) 
+    {
+        // もし自分自身宛てだったら無視する.(無限ループになっちゃうから)
+        if ($re->user->screen_name === 'chomado_bot')
+            continue;
+        
+        // ツイート本文
+        $chat       = new Chat($re->user->screen_name, $re->user->name, $re->text);
+        $message    = sprintf('%s%s%s%s'
+            , $reply_list[$index]
+            , PHP_EOL
+            , $chat->ResText()
+            , PHP_EOL
+            );
 
-		$param['status'] = sprintf('@%s %sさん%s%s'
-			, $re->user->screen_name
-			, trim(preg_replace('!([@＠#＃.]|://)!u', " $1 ", $re->user->name))
-			, PHP_EOL
-			, $message
-			);
-		$index = $index < count($reply_list) - 1 ? $index + 1 : 0;
+        $param['status'] = sprintf('@%s %sさん%s%s'
+            , $re->user->screen_name
+            , trim(preg_replace('!([@＠#＃.]|://)!u', " $1 ", $re->user->name))
+            , PHP_EOL
+            , $message
+            );
+        $index = $index < count($reply_list) - 1 ? $index + 1 : 0;
 
-		$param['in_reply_to_status_id'] = $re->id_str;
-		// 投稿
-		$connection->post('statuses/update', $param);
-	}
-	// 最終投稿IDを書き込む
-	file_put_contents('tweet_content_data_list/last_id.txt', $res[0]->id_str);
+        $param['in_reply_to_status_id'] = $re->id_str;
+        // 投稿
+        $connection->post('statuses/update', $param);
+    }
+    // 最終投稿IDを書き込む
+    file_put_contents('tweet_content_data_list/last_id.txt', $res[0]->id_str);
 }
