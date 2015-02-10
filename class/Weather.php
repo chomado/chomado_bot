542fead72f61261f6e1a4713e15fce242400a9e6
AIZAWA, Hina <hina@bouhime.com>
2015-02-10 16:36:06 +0900

    use curl

--- class/Weather.php
+++ class/Weather.php
@@ -1,6 +1,7 @@
 <?php
 namespace bot;
 use bot\weather\Dictionary;
+use Curl\Curl;
 
 /**
  * 天気情報取得クラス
@@ -8,24 +9,24 @@ use bot\weather\Dictionary;
 class Weather
 {
     /** @var string 天気情報を取得する対象の都市 */
-	private $city;
+    private $city;
 
     /**
      * @var stdClass 取得した天気情報
      * @see GetWeather()
      */
-	private $info;
+    private $info;
 
     /**
      * コンストラクタ
      *
      * @param   string  $city   対象の都市の名前(例: tokyo)
      */
-	public function __construct($city)
-	{
-		$this->city 	= $city;
-		$this->info 	= $this->GetWeather();// API呼び出しを1回で済ませるためにここでgetしておく
-	}
+    public function __construct($city)
+    {
+        $this->city = $city;
+        $this->info = $this->GetWeather();// API呼び出しを1回で済ませるためにここでgetしておく
+    }
 
     /**
      * 華氏から摂氏に変換する
@@ -33,57 +34,74 @@ class Weather
      * @param   float   $f  華氏温度
      * @return  float       摂氏温度
      */
-	private function FtoC($f)
-	{
+    private function FtoC($f)
+    {
         return ((double)$f - 32) * 5 / 9;
-	}
+    }
 
     /**
      * 現在の天気の情報を問い合わせる
      *
-     * @todo: クエリは当然失敗するかもしれないのでエラー処理が必要
+     * @return stdClass
+     * @throws \Exception 問い合わせに失敗した場合
+     *
      * @todo: YQLに埋め込まれるパラメータのエスケープ方法が不明
      */
-	private function GetWeather()
-	{
-		$parameters = [
-			'q' 		=> sprintf(
-							'select * from weather.forecast where woeid in (select woeid from geo.places(1) where text="%s")'
-							, $this->city), //FIXME: cityのエンコード方法は?
-			'format' 	=> 'json',
-			'env' 		=> 'store://datatables.org/alltableswithkeys',
-		];
-		$query_uri = 'https://query.yahooapis.com/v1/public/yql?'
-					. http_build_query($parameters, '', '&', PHP_QUERY_RFC3986);
-		return json_decode(file_get_contents($query_uri))->query->results->channel->item;
-	}
+    private function GetWeather()
+    {
+        $parameters = [
+            'q'         => sprintf(
+                                'select * from weather.forecast where woeid in (select woeid from geo.places(1) where text="%s")'
+                                , $this->city), //FIXME: cityのエンコード方法は?
+            'format'    => 'json',
+            'env'       => 'store://datatables.org/alltableswithkeys',
+        ];
+        $query_uri = 'https://query.yahooapis.com/v1/public/yql?'
+                   . http_build_query($parameters, '', '&', PHP_QUERY_RFC3986);
+        
+        Log::info(__METHOD__ . ': Query URL: ' . $query_uri);
+        $curl = new Curl();
+        $resp = $curl->get($query_uri);
+        if($curl->error) {
+            $msg = 'YQL Query Error: ' . $curl->error_code . ': ' . $curl->error_message;
+            Log::error(__METHOD__ . ': ' . $msg);
+            Log::error($curl->raw_response);
+            throw new \Exception($msg);
+        }
+        if(!is_object($resp) || !isset($resp->query->results->channel->item)) {
+            Log::error('YQL was returned unexpected response');
+            Log::error($curl->raw_response);
+            throw new \Exception('YQL was returned unexpected response');
+        }
+        return $resp->query->results->channel->item;
+    }
 
     /**
      * 現在の天気を取得
      *
      * @return array
      */
-	private function GetCondition()
-	{
-		return [
-			'weather'	=> Dictionary::GetJapanese($this->info->condition->code, $this->info->forecast[1]->text)
-			, 'temp'	=> $this->FtoC($this->info->condition->temp)
-		];
-	}
+    private function GetCondition()
+    {
+        return [
+            'weather'   => Dictionary::GetJapanese($this->info->condition->code, $this->info->forecast[1]->text)
+            , 'temp'    => $this->FtoC($this->info->condition->temp)
+        ];
+    }
 
     /**
      * 明日の天気を取得
      *
      * @return array
      */
-	private function GetTomorrow()
-	{
-		return [
-			'weather'	=> Dictionary::GetJapanese($this->info->forecast[1]->code, $this->info->forecast[1]->text)
-			, 'high'	=> $this->FtoC($this->info->forecast[1]->high)
-			, 'low'		=> $this->FtoC($this->info->forecast[1]->low)
-		];
-	}
+    private function GetTomorrow()
+    {
+        return [
+            'weather'   => Dictionary::GetJapanese($this->info->forecast[1]->code, $this->info->forecast[1]->text)
+            , 'high'    => $this->FtoC($this->info->forecast[1]->high)
+            , 'low'     => $this->FtoC($this->info->forecast[1]->low)
+        ];
+    }
 
     /**
      * 天気情報を成形して取得する
@@ -95,13 +113,13 @@ class Weather
      * @todo    APIで取得してきた情報は現在時間のものではない
      * @todo    現在時間なら $time はそもそも不要
      */
-	public function GetWeatherMessage($time)
-	{
-		$now 	  = $this->GetCondition();
-		$tomorrow = $this->GetTomorrow();
+    public function GetWeatherMessage($time)
+    {
+        $now        = $this->GetCondition();
+        $tomorrow   = $this->GetTomorrow();
 
-		$message = sprintf(
-			'東京の現在(%s)の天気は、%s(%.1f℃)です。%s明日は%sで、最高気温%.1f℃、最低気温%.1f℃です。%s'
+        $message = sprintf(
+            '東京の現在(%s)の天気は、%s(%.1f℃)です。%s明日は%sで、最高気温%.1f℃、最低気温%.1f℃です。%s'
             , $time->format('m/d H:i')
             , $now['weather']
             , $now['temp']
@@ -111,5 +129,5 @@ class Weather
             , $tomorrow['low']
             , PHP_EOL);
         return $message;
-	}
+    }
 }
